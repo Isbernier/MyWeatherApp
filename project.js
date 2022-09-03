af946fc0586dcca033c2b476bdba9b666d4f8957
Isbernier <103043999+Isbernier@users.noreply.github.com>
2022-09-03 07:36:37 +0200

    MyWeatherApp
    Index, JS, Stylesheet and Image file imported

---
+++ project.js
@@ -0,0 +1,39 @@
+let now = new Date();
+let minutes = now.getMinutes();
+if (minutes < 10) minutes = "0" + minutes;
+let hour = now.getHours();
+let day = now.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+let weekDay = days[day];
+
+let formattedDate = `Today ${weekDay}, ${hour}:${minutes}`;
+
+let h3 = document.querySelector("#today");
+h3.innerHTML = formattedDate;
+//end of date
+
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#rightNow");
+  temperatureElement.innerHTML = `Right now: ${temperature}ºC`;
+}
+
+function searchCity(event) {
+  event.preventDefault();
+
+  let citySearched = document.querySelector("#search-text-input");
+  citySearched = citySearched.value;
+
+  let h2city = document.querySelector("#h2city");
+  h2city.innerHTML = citySearched;
+
+  let units = "metric";
+  let apiKey = "b0684348b73ea07b122cc59301878b16";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearched}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+let h2city = document.querySelector("#search-form");
+h2city.addEventListener("submit", searchCity);
